3638bf11780cf54b1f0feadb487e1ad48a51db0f
AlgO <alexandre.brethes@gmail.com>
2019-11-29 10:33:57 +0100

    Buttons WORKING

--- Assets/VW_Assets/Scripts/ButtonHandler.cs
+++ Assets/VW_Assets/Scripts/ButtonHandler.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-/// <summary>
-/// This script shall be added as a component on the ControlPanel.
-/// It references the buttons displayed on the Control Panel and their alternative states.
-/// Finally it stores the Scene Setup IDs and make them accessible for the SceneOrganizer.
-/// </summary>
 
+
+[System.Serializable]
 struct SetupButton
 {
     public GameObject button;
     public bool buttonState;
 }
 
+/// <summary>
+/// This script shall be added as a component on the ControlPanel.
+/// It references the buttons displayed on the Control Panel and their alternative states.
+/// Finally it stores the Scene Setup IDs and make them accessible for the SceneOrganizer.
+/// </summary>
 public class ButtonHandler : MonoBehaviour
 {
     [SerializeField]
@@ -25,6 +27,9 @@ public class ButtonHandler : MonoBehaviour
 
 
     Dictionary<GameObject, bool> _buttonList;
+    List<GameObject> _buttonGameObjectsList;
+    List<bool> _buttonStatesList;
+
 
     [SerializeField]
     Material _buttonActivatedMaterial;
@@ -33,23 +38,32 @@ public class ButtonHandler : MonoBehaviour
 
     void Start()
     {
+        _buttonList = new Dictionary<GameObject, bool>();
+        _buttonGameObjectsList = new List<GameObject>();
+        _buttonStatesList = new List<bool>();
         foreach (SetupButton s in _setupButtons)
         {
+            Debug.Log("Button " + s.button);
+            Debug.Log("ButtonList " + _buttonList);
+           
             _buttonList.Add(s.button, s.buttonState);
+            _buttonGameObjectsList.Add(s.button);
+            _buttonStatesList.Add(s.buttonState);
         }
     }
 
     void UpdateButtonState()
     {
-        foreach (GameObject g in _buttonList.Keys)
+        
+        foreach (GameObject g in _buttonGameObjectsList)
         {
             _buttonList[g] = false;
             g.GetComponent<Renderer>().material = _buttonDeactivatedMaterial;
         }
-        foreach (GameObject g in _buttonList.Keys)
+        foreach (GameObject g in _buttonGameObjectsList)
         {
             ButtonTrigger bt = g.GetComponent<ButtonTrigger>();
-            if (bt.enabled  && !_buttonList.ContainsValue(true))
+            if (bt.enabled && !_buttonList.ContainsValue(true))
             {
                 _buttonList[g] = true;
                 g.GetComponent<Renderer>().material = _buttonActivatedMaterial;
