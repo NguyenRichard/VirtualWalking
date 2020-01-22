f179f4cac22a0277b4eb8d7343f51252a8677849
AlgO <alexandre.brethes@gmail.com>
2020-01-22 11:17:39 +0100

    Dynamo + Début Guardian

--- Assets/VW_Assets/Scripts/SpeedIndicator/SpeedIndicator.cs
+++ Assets/VW_Assets/Scripts/SpeedIndicator/SpeedIndicator.cs
@@ -11,19 +11,25 @@ public class SpeedIndicator : MonoBehaviour
     [SerializeField] float minimalSpeed = (4.0f / 3.6f);
     [SerializeField] float maximalSpeed = (8.0f / 3.6f); // 4 kilomètres par heures exprimé en mètres par seconde
     public float speedIndicator = 0.0f; // Varie entre 0 et 1
-    Vector3 OldPosition;
-    float Distance;
+    Vector3 _oldPosition;
+    float _distance;
 
     void Start()
     {
-        OldPosition = this.transform.position;
+        _oldPosition = this.transform.position;
     }
 
+    // V2, l'autre me semble pas ouf
     void Update()
     {
-        Distance = Vector3.Distance(OldPosition, this.transform.position);
-        speedIndicator = (speedIndicator + (Mathf.Clamp(Distance/Time.deltaTime, minimalSpeed, maximalSpeed) / (maximalSpeed - minimalSpeed)))/2.0f;
-        OldPosition = this.transform.position;
-        speedIndicator = (float)((int)(speedIndicator * 10))/10.0f;
+        //_distance = Vector3.Distance(_oldPosition, this.transform.position);
+        //speedIndicator = (speedIndicator + (Mathf.Clamp(_distance/Time.deltaTime, minimalSpeed, maximalSpeed) / (maximalSpeed - minimalSpeed)))/2.0f;
+        //_oldPosition = this.transform.position;
+        //speedIndicator = (float)((int)(speedIndicator * 10))/10.0f;
+
+        _distance = Vector3.Distance(_oldPosition, transform.position);
+        speedIndicator = Mathf.Lerp(speedIndicator, _distance/((maximalSpeed - minimalSpeed) * Time.deltaTime), Time.deltaTime);
+        Mathf.Clamp(speedIndicator, 1, 0);
+        _oldPosition = transform.position;
     }
 }
